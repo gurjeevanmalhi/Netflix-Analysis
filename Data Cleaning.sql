e55779b721ed8d928d9c43236728d539a51e0556
Gurjeevan <gurjeevanmalhi@outlook.com>
2025-04-17 19:30:19 -0500

    Added Business Problems.sql and updated Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,151 +1,178 @@
--- Data Cleaning and Manipulation
+-- Data Cleaning
 
--- 1. Replacing all NULL and blank values with 'Unknown' (director, casts, country, date_added, rating, duration)
+-- Replacing NULL and blanks with "Unknown"
 
 update netflix_titles
 set 
-    director = case
-        when director is null or LTRIM(RTRIM(director)) = ' ' then 'Unknown'
+    director = case 
+        when director is null or trim(director) = ' ' then 'Unknown'
         else director
         end,
     casts = case 
-        when casts is null or LTRIM(RTRIM(casts)) = ' ' then 'Unknown'
+        when casts is null or trim(casts)= ' ' then 'Unknown'
         else casts 
         end,
     country = case 
-        when country is null or LTRIM(RTRIM(country)) = ' ' then 'Unknown'
+        when country is null or trim(country) = ' ' then 'Unknown'
         else country
         end,
     rating = case 
-        when rating is null or LTRIM(RTRIM(rating)) = ' ' then 'Unknown'
-        else rating 
+        when rating is null or trim(rating) = ' ' then 'Unknown'
+        else rating
         end,
     duration = case 
-        when duration is null or LTRIM(RTRIM(duration)) = ' ' then 'Unknown'
+        when duration is null or trim(duration) = ' ' then 'Unknown'
         else duration
+        end,
+    listed_in = case 
+        when listed_in is null or trim(listed_in) = ' ' then 'Unknown'
+        else listed_in
+        end,
+    title_description = case 
+        when title_description is null or trim(title_description) = ' ' then 'Unknown'
+        else title_description
         end;
+    
 
--- 2. Create table for casts
+-- Normalization
 
-create table casts(
-    show_id NVARCHAR(10),
-    cast_member NVARCHAR(100)
+-- Creating casts table
+
+create table cast_list(
+    cast_id int primary key identity(1,1),
+    cast_member nvarchar(255)
 );
--- 3. Insert into for casts
 
-insert into casts (show_id, cast_member)
-select 
-    show_id,
-    trim(value) as cast_member
+-- Insert into cast table
+
+insert into cast_list(cast_member)
+select distinct trim(split_cast.value)
 from netflix_titles
-cross apply string_split(casts, ',')
+cross apply string_split(casts,',') as split_cast
 where casts <> 'Unknown';
 
--- 4. Create table for country
+-- Creating junction table (show-cast relationship)
+
+create table show_cast(
+    show_id nvarchar(10),
+    cast_id int,
+    foreign key (show_id) references netflix_titles(show_id),
+    foreign key (cast_id) references cast_list(cast_id)
+);
+
+-- Insert into show_cast
+
+insert into show_cast(show_id,cast_id)
+select
+    nt.show_id,
+    c.cast_id
+from netflix_titles as nt
+cross apply string_split(casts,',') as cast_member_split
+join cast_list as c 
+    on trim(cast_member_split.value) = c.cast_member;
+
+-- Creating countries table table
 
 create table countries(
-    show_id NVARCHAR(10),
-    country NVARCHAR(50)
+    country_id int primary key identity (1,1),
+    country_name varchar(50)
 );
 
--- 5. Insert into for country
 
-insert into countries(show_id, country)
+-- Insert into countries table (see view windows query for cleaning syntax)
+
+insert into countries(country_name)
+select distinct clean_country
+from vw_clean_countries;
+
+-- Creating junction table (show_country relationship)
+
+create table show_country(
+    show_id nvarchar(10),
+    country_id int,
+    foreign key (show_id) references netflix_titles(show_id),
+    foreign key (country_id) references countries(country_id)
+);
+
+-- Insert into show_country
+
+insert into show_country(show_id, country_id)
 select
-    show_id,
-    trim(value) as country 
-from netflix_titles
-cross apply string_split(country, ',')
-where country <> 'Unknown';
+    vwc.show_id,
+    c2.country_id
+from vw_clean_countries as vwc
+join countries as c2 
+    on vwc.clean_country = c2.country_name;
 
--- 6. Create table for genres
+-- Creating genres table
 
-create table genres(
-    show_id NVARCHAR(10),
-    genre NVARCHAR(50)
+create table genre(
+    genre_id int primary key identity (1,1),
+    genre_name nvarchar(50)
+);
+-- Insert into genre table
+
+insert into genre(genre_name)
+select
+    distinct clean_genre
+from vw_clean_genres;
+
+-- Creating junction table (show-genre relationship)
+
+create table show_genre(
+    show_id nvarchar(10),
+    genre_id int
+    foreign key (show_id) references netflix_titles (show_id),
+    foreign key (genre_id) references genre(genre_id)
 );
 
--- 7. Insert into genres
+-- Inserting into show_genre (see view windows query for cleaning syntax)
 
-insert into genres(show_id,genre)
-select 
-    show_id,
-    trim(value) as genre
-from netflix_titles
-cross apply string_split(listed_in, ',')
-where listed_in is not null 
-    or listed_in <> 'Unknown';
+insert into show_genre(show_id, genre_id)
+select
+    vw.show_id,
+    g.genre_id
+from vw_clean_genres as vw
+join genre as g 
+    on vw.clean_genre = g.genre_name;
 
--- 8. Normalize genres
+---- Creating columns to normalize duration
 
-update genres
-set genre = 
-    case 
-        when genre like '%Crime%' then 'Crime'
-        when genre like '%Drama%' then 'Drama'
-        when genre like '%International%' then 'International'
-        when genre like '%Korean%' then 'International'
-        when genre like '%Spanish%' then 'International'
-        when genre like '%British%' then 'International'
-        when genre like '%Roman%' then 'Romance'
-        when genre like '%Comed%' then 'Comedy'
-        when genre like '%Docu%' then 'Documentaries'
-        when genre like '%Horror%' then 'Horror'
-        when genre like '%Thrill%' then 'Thrillers'
-        when genre like '%Myster%' then 'Mystery'
-        when genre like '%Action & Adventure%' then 'Action'
-        when genre like '%Music%' then 'Musicals'
-        when genre like '%Classic%' then 'Classic'
-        when genre like '%Cult%' then 'Classic'
-        when genre like '%Kids%' then 'Children & Family'
-        when genre like '%Children%' then 'Children & Family'
-        when genre like '%Anime%' then 'Anime'
-        when genre like '%Independ%' then 'Independent'
-        when genre like '%Teen%' then 'Teen'
-        when genre like '%Sci-Fi%' then 'Sci-Fi'
-        when genre like '%Science%' then 'Science & Nature'
-        when genre like '%Faith%' then 'Religion'
-        when genre like '%Reality%' then 'Reality'
-        when genre like '%Sport%' then 'Sports'
-        when genre like '%LGBTQ%' then 'LGBTQ'
-        else genre
-    end;
-
--- 9. Normalize duration column
-
-alter table netflix_titles -- adding column for value of duration of unit of measurement (minutes or season)
+alter table netflix_titles
 add
     duration_value int,
-    duration_unit NVARCHAR(10);
+    duration_type nvarchar(20);
 
-update netflix_titles -- extracting from duration column into new columns
-set
-    duration_value = case 
-        when CHARINDEX(' ',duration) > 0
-        then try_cast(left(duration, CHARINDEX(' ',duration)-1)as int)
-        else null 
-    end,
-    duration_unit = case 
-        when CHARINDEX(' ',duration) > 0
-        then LTRIM(RIGHT(duration,len(duration)-CHARINDEX(' ',duration)))
-        else null
-    end;
+-- Extract from duration and update into duration value and duration type
 
 update netflix_titles
-set
-    duration_unit = case
-        when duration_unit like '%Seas%' then 'Season'
-        else duration_unit
+set 
+    duration_value = left(duration,CHARINDEX(' ',duration, -1)), -- finds position before space and returns all string to the left
+    duration_type =right(duration,LEN(duration) - CHARINDEX(' ',duration)); -- subtracts position of space from total string length and returns all string to the right
+
+-- Normalizing strings for duration type
+
+update netflix_titles
+set 
+    duration_type = case 
+        when duration_type like '%min%' then 'minute'
+        when duration_type like '%seas%' then 'season'
+        else duration_type
         end;
 
--- 10. Drop columns no longer needed
-select * from netflix_titles;
-alter table netflix_titles
-drop column
-    casts,
-    country,
-    duration;
+-- Removing columns from netflix table
 
 alter table netflix_titles
 drop column 
+    casts,
+    country,
+    duration,
     listed_in;
+
+
+
+
+
+
+
+
